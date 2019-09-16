eeb92fcc9bf55316fe228a3ebddc684fbe71b767
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2019-09-16 14:32:58 +0100

    Update hello-world.js code structure

--- src/projects/01-hello-world/hello-world.js
+++ src/projects/01-hello-world/hello-world.js
@@ -3,12 +3,6 @@ const lastName = prompt('Now enter your last name');
 const age = prompt('Great! What is your current age?');
 
 alert(
-  'Nice to meet you,'
-  + firstName
-  + ' '
-  + lastName
-  + '!\n'
-  + 'You are aged '
-  + age
-  + ' years old.',
+  `Nice to meet you, ${firstName} ${lastName}!\n` +
+    `You are aged ${age} years old.`
 );
